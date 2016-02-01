71067eac0200d8697be6caf263aa7c7fca6de651
Kenny Williams <kennyjwilli@gmail.com>
2016-01-31 17:25:05 -0800

    Remove postal ws connection

--- src/postal_messenger/client/portal.cljs
+++ src/postal_messenger/client/portal.cljs
@@ -2,14 +2,11 @@
   (:require [rum.core :as rum]
             [griebenschmalz.core :refer [do!] :as g]
             [postal-messenger.client.util.misc :as misc]
-            [postal.client :as pc]
             [pusher.core :as pusher]
             [beicon.core :as s]
             [promesa.core :as p]
             [postal-messenger.client.util.http :as http]
             [cljs.reader :as reader]))
-
-(def client (pc/client (str (misc/url-origin) "/postal")))
 
 (defn- message-handler
   [msg message-bus]
@@ -29,15 +26,10 @@
 
 (def subscribe-on-mount
   {:did-mount (fn [state]
-                (let [message-bus (-> state :rum/args first)
-                      [instream outbus] (pc/socket client :messages {:token (misc/get-jwt)})]
-                  (s/on-value instream (fn [message]
-                                         (println "MESSAGE" message)))
-                  (s/on-end instream #(.info js/console "Message websocket closed"))
+                (let [message-bus (-> state :rum/args first)]
                   (p/then (http/get! "/api/pusher")
                           (fn [{body :body}]
                             (connect-pusher (:message-channel body) (:api-key body) message-bus)))
-                  (do! message-bus #(assoc % :messages-bus outbus))
                   state))})
 
 (rum/defc root < subscribe-on-mount
