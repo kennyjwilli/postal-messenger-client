08c6d95e7d311d4a7c4886ddd7b087ac32f606d2
Kenny Williams <kennyjwilli@gmail.com>
2016-02-12 00:48:50 -0800

    Refactor out conversation-item to be dynamic

--- src/postal_messenger/client/views/core.cljs
+++ src/postal_messenger/client/views/core.cljs
@@ -1,36 +1,21 @@
 (ns postal-messenger.client.views.core
   (:require [rum.core :as rum]
             [griebenschmalz.core :refer [do!] :as g]
+            [cats.labs.lens :as l]
             [cljs-time.core :as t]
-            [postal-messenger.client.util.messaging :as msg]))
+            [postal-messenger.client.util.messaging :as msg]
+            [postal-messenger.client.util.misc :as misc]))
 
-(def conversations
-  {(msg/conversation-id [{:phone      "4445556666"
-                          :first-name "Mary"
-                          :last-name  "Smith"}])
-   {:recipients  [{:phone      "4445556666"
-                   :first-name "Mary"
-                   :last-name  "Smith"}]
-    :last-update (t/date-time 2016 2 10 8 13)
-    :messages    [{:data      "mary message"
-                   :type      "sent"
-                   :timestamp 1}
-                  {:data      "recieved mary message"
-                   :type      "received"
-                   :timestamp 2}]}
-   (msg/conversation-id [{:phone      "1112223333"
-                          :first-name "John"
-                          :last-name  "Example"}])
-   {:recipients  [{:phone      "1112223333"
-                   :first-name "John"
-                   :last-name  "Example"}]
-    :last-update (t/date-time 2016 2 11 10 55)
-    :messages    [{:data      "first"
-                   :type      "sent"
-                   :timestamp 1}
-                  {:data      "second"
-                   :type      "received"
-                   :timestamp 2}]}})
+(rum/defc conversation-item
+          [message-bus state id]
+          [:div {:class "conv noselect pointer"
+                 :key   (str "conv" id)}
+           [:div.avatar {:style {:background-image (str "url(img/walter-white.jpg)")}}]
+           [:div
+            [:div.layout.horizontal
+             [:div.flex {:class "conv-title one-line-text"} (misc/format-recipients (:recipients state))]
+             [:div {:class "last-update"} "10:59 PM"]]
+            [:span {:class "last-message one-line-text"} (-> state :messages last misc/msg-text)]]])
 
 (rum/defc root
           [message-bus state]
@@ -40,13 +25,7 @@
             [:div {:class "horz-center conv-container"}
              [:div.layout.horizontal.fit
               [:div {:class "conv-list"}
-               [:div {:class "conv"}
-                [:div.avatar {:style {:background-image (str "url(img/walter-white.jpg)")
-                                      :float            "left"}}]
-                [:div
-                 [:div.layout.horizontal
-                  [:div.flex {:class "conv-title one-line-text"} "Lindsey Cakebread"]
-                  [:div {:class "last-update"} "10:59 PM"]]
-                 ;;TODO: Figure out stupid text wrapping problem
-                 [:span {:class "last-message one-line-text"} "Rude to Dixie! sdf sdf sdf dsfdfdf egtr gfsdfds sdfsdfsdfdsf"]]]]
+               (map (fn [[id conv]]
+                      (g/lensed-component (l/in [:conversations id]) conversation-item message-bus state id))
+                    (-> state :conversations msg/sort-conversations))]
               [:div.flex {:style {:background-color "purple"}}]]]]])
