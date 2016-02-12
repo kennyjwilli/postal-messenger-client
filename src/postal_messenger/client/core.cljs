4af2cb34eed3c4b47f5817ca6acd463995e41c08
Kenny Williams <kennyjwilli@gmail.com>
2016-02-12 00:47:59 -0800

    Add some dummy conversations to the state

--- src/postal_messenger/client/core.cljs
+++ src/postal_messenger/client/core.cljs
@@ -2,17 +2,41 @@
   (:require [griebenschmalz.core :as g]
             [rum.core :as rum]
             [cemerick.url :refer [url]]
-            #_[postal-messenger.client.portal :refer [root]]
+    #_[postal-messenger.client.portal :refer [root]]
             [postal-messenger.client.views.core :as view]
-            [postal-messenger.client.util.misc :as misc]))
+            [postal-messenger.client.util.misc :as misc]
+            [postal-messenger.client.util.messaging :as msg]
+            [cljs-time.core :as t]))
 
 (def initial-state
-  {:input-value ""
-   :socket_id   ""
-   :messages    [{:data "first"
-                  :type "sent"}
-                 {:data "second"
-                  :type "received"}]})
+  {:input-value   ""
+   :socket_id     ""
+   :conversations {(msg/conversation-id [{:phone      "4445556666"
+                                          :first-name "Mary"
+                                          :last-name  "Smith"}])
+                   {:recipients  [{:phone      "4445556666"
+                                   :first-name "Mary"
+                                   :last-name  "Smith"}]
+                    :last-update (t/date-time 2016 2 10 8 13)
+                    :messages    [{:data      "mary message"
+                                   :type      "sent"
+                                   :timestamp 1}
+                                  {:data      "recieved mary message"
+                                   :type      "received"
+                                   :timestamp 2}]}
+                   (msg/conversation-id [{:phone      "1112223333"
+                                          :first-name "John"
+                                          :last-name  "Example"}])
+                   {:recipients  [{:phone      "1112223333"
+                                   :first-name "John"
+                                   :last-name  "Example"}]
+                    :last-update (t/date-time 2016 2 11 10 55)
+                    :messages    [{:data      "first"
+                                   :type      "sent"
+                                   :timestamp 1}
+                                  {:data      "second"
+                                   :type      "received"
+                                   :timestamp 2}]}}})
 
 (defn render-fn
   [message-bus state]
