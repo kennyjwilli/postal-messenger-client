822d7e5064528b0a028fc52ba1806951defa7be1
Kenny Williams <kennyjwilli@gmail.com>
2016-02-11 21:25:49 -0800

    Added conversation sorting

--- src/postal_messenger/client/util/messaging.cljs
+++ src/postal_messenger/client/util/messaging.cljs
@@ -1,5 +1,26 @@
 (ns postal-messenger.client.util.messaging
-  (:require [postal-messenger.client.util.http :as http]))
+  (:require [postal-messenger.client.util.http :as http]
+            [cljs-time.core :as t]))
+
+(defn conversation-id
+  "Returns a unique id for a given set of recipients."
+  [recipients]
+  (hash (sort-by hash recipients)))
+
+(defn time-comparator
+  "Sort time by closest time first"
+  [x y]
+  (cond
+    (t/equal? x y) 0
+    (t/after? x y) -1
+    :default 1))
+
+(defn sort-conversations
+  "Sorts conversations by the newest conversation first"
+  [convs]
+  (into (sorted-map-by (fn [x y]
+                         (time-comparator (get-in convs [x :last-update])
+                                          (get-in convs [y :last-update])))) convs))
 
 (defn send-message!
   [socket_id sender data]
