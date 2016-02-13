94583fceb519d986b4da333edc8cd22ac0bcb2e3
Kenny Williams <kennyjwilli@gmail.com>
2016-02-12 17:29:24 -0800

    Add input box for sending messages

--- src/postal_messenger/client/core.cljs
+++ src/postal_messenger/client/core.cljs
@@ -8,35 +8,38 @@
             [postal-messenger.client.util.messaging :as msg]
             [cljs-time.core :as t]))
 
+(enable-console-print!)
+
 (def initial-state
-  {:input-value   ""
-   :socket_id     ""
-   :conversations {(msg/conversation-id [{:phone      "4445556666"
-                                          :first-name "Mary"
-                                          :last-name  "Smith"}])
-                   {:recipients  [{:phone      "4445556666"
-                                   :first-name "Mary"
-                                   :last-name  "Smith"}]
-                    :last-update (t/date-time 2016 2 10 8 13)
-                    :messages    [{:data      "mary message"
-                                   :type      "sent"
-                                   :timestamp 1}
-                                  {:data      "recieved mary message"
-                                   :type      "received"
-                                   :timestamp 2}]}
-                   (msg/conversation-id [{:phone      "1112223333"
-                                          :first-name "John"
-                                          :last-name  "Example"}])
-                   {:recipients  [{:phone      "1112223333"
-                                   :first-name "John"
-                                   :last-name  "Example"}]
-                    :last-update (t/date-time 2016 2 11 10 55)
-                    :messages    [{:data      "first"
-                                   :type      "sent"
-                                   :timestamp 1}
-                                  {:data      "second"
-                                   :type      "received"
-                                   :timestamp 2}]}}})
+  {:input-value           ""
+   :socket_id             ""
+   :selected-conversation nil
+   :conversations         {(msg/conversation-id [{:phone      "4445556666"
+                                                  :first-name "Mary"
+                                                  :last-name  "Smith"}])
+                           {:recipients  [{:phone      "4445556666"
+                                           :first-name "Mary"
+                                           :last-name  "Smith"}]
+                            :last-update (t/date-time 2016 2 10 8 13)
+                            :messages    [{:data      "mary message"
+                                           :type      "sent"
+                                           :timestamp 1}
+                                          {:data      "recieved mary message"
+                                           :type      "received"
+                                           :timestamp 2}]}
+                           (msg/conversation-id [{:phone      "1112223333"
+                                                  :first-name "John"
+                                                  :last-name  "Example"}])
+                           {:recipients  [{:phone      "1112223333"
+                                           :first-name "John"
+                                           :last-name  "Example"}]
+                            :last-update (t/date-time 2016 2 12 10 55)
+                            :messages    [{:data      "first"
+                                           :type      "sent"
+                                           :timestamp 1}
+                                          {:data      "second"
+                                           :type      "received"
+                                           :timestamp 2}]}}})
 
 (defn render-fn
   [message-bus state]
@@ -44,7 +47,6 @@
 
 (defn ^:export main
   []
-  (enable-console-print!)
   (let [jwt "" #_(misc/get-jwt)]
     (if (nil? jwt)
       (let [current-url (url (misc/get-url))
--- src/postal_messenger/client/views/core.cljs
+++ src/postal_messenger/client/views/core.cljs
@@ -3,19 +3,26 @@
             [griebenschmalz.core :refer [do!] :as g]
             [cats.labs.lens :as l]
             [cljs-time.core :as t]
-            [postal-messenger.client.util.messaging :as msg]
+            [postal-messenger.client.util.messaging :as m]
             [postal-messenger.client.util.misc :as misc]))
 
 (rum/defc conversation-item
           [message-bus state id]
-          [:div {:class "conv noselect pointer"
-                 :key   (str "conv" id)}
-           [:div.avatar {:style {:background-image (str "url(img/walter-white.jpg)")}}]
-           [:div
-            [:div.layout.horizontal
-             [:div.flex {:class "conv-title one-line-text"} (misc/format-recipients (:recipients state))]
-             [:div {:class "last-update"} (misc/format-time (:last-update state))]]
-            [:span {:class "last-message one-line-text"} (-> state :messages last misc/msg-text)]]])
+          (let [conv (get-in state [:conversations id])]
+            [:div {:class    "conv noselect pointer"
+                   :key      (str "conv" id)
+                   :on-click (fn [] (do! message-bus #(assoc % :selected-conversation id)))}
+             [:div.avatar {:style {:background-image (str "url(img/walter-white.jpg)")}}]
+             [:div
+              [:div.layout.horizontal
+               [:div.flex {:class "conv-title one-line-text"} (misc/format-recipients (:recipients conv))]
+               [:div {:class "last-update"} (misc/format-time (:last-update conv))]]
+              [:span {:class "last-message one-line-text"} (-> conv :messages last misc/msg-text)]]]))
+
+(rum/defc message
+          [message-bus msg]
+          [:div {:class (str (:type msg) " message")}
+           (:data msg)])
 
 (rum/defc root
           [message-bus state]
@@ -26,6 +33,23 @@
              [:div.layout.horizontal.fit
               [:div {:class "conv-list"}
                (map (fn [[id conv]]
-                      (g/lensed-component (l/in [:conversations id]) conversation-item message-bus state id))
-                    (-> state :conversations msg/sort-conversations))]
-              [:div.flex {:style {:background-color "purple"}}]]]]])
+                      (conversation-item message-bus state id)
+                      #_(g/lensed-component (l/in [:conversations id]) conversation-item message-bus state id))
+                    (-> state :conversations m/sort-conversations))]
+              [:div {:class "flex conv-messages"}
+               [:div.fit
+                [:div.layout.vertical
+                 [:div {:class "message-list-container flex"}
+                  (let [messages (get-in state [:conversations (:selected-conversation state) :messages])]
+                    (map (fn [msg]
+                           (message message-bus msg)) messages))]
+                 [:div {:class "new-message-container"}
+                  [:div.layout.vertical.center-justified {:style {:height "100%"}}
+                   [:div.layout.horizontal
+                    [:span {:class           "flex"
+                            :placeholder     "Send a message"
+                            :contentEditable true
+                            :on-input        (fn [e]
+                                               (do! message-bus #(assoc % :input-value (aget e "target" "outerText"))))}]
+                    [:div.layout.vertical.center-justified
+                     [:i {:class "zmdi zmdi-mail-send"}]]]]]]]]]]]])
