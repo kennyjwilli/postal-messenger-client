6ab4bfdf0dfa7a1601b1117d87d2734dfdc0a855
Kenny Williams <kennyjwilli@gmail.com>
2016-02-14 00:42:49 -0800

    Implement notification api

---
+++ src/postal_messenger/client/util/notification.cljs
@@ -0,0 +1,29 @@
+(ns postal-messenger.client.util.notification)
+
+(def notification (aget js/window "Notification"))
+
+(defn support-notifications?
+  []
+  (some? notification))
+
+(defn request-permission
+  [callback]
+  (.. notification requestPermission (then (fn [result]
+                                             (callback (keyword result))))))
+
+(defn has-permission?
+  []
+  (= (aget notification "permission") "granted"))
+
+(defn close
+  [notif]
+  (.close notif))
+
+(defn notify
+  ([title] (notify title {}))
+  ([title {:keys [timeout on-click on-error] :or {timeout 4000} :as opts}]
+   (let [n (js/Notification. title (clj->js opts))]
+     (aset n "onclick" on-click)
+     (aset n "onerror" on-error)
+     (js/setTimeout (.. n -close (bind n)) timeout)
+     n)))
