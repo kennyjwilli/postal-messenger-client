bf53d0bd4ef21ff74b9f3a12a8395a24c7a7abd3
Kenny Williams <kennyjwilli@gmail.com>
2016-02-25 00:17:26 -0800

    Change send-event! fn to follow event schema. Add get-conversations!

--- src/postal_messenger/client/util/messaging.cljs
+++ src/postal_messenger/client/util/messaging.cljs
@@ -1,51 +1,25 @@
 (ns postal-messenger.client.util.messaging
-  (:require [postal-messenger.client.util.http :as http]
-            [cljs-time.core :as t]
-            [cljs-time.coerce :as ct]
-            [cljs-time.format :as fmt]))
-
-(defn conversation-id
-  "Returns a unique id for a given set of recipients."
-  [recipients]
-  (hash (sort-by hash recipients)))
-
-(defn time-comparator
-  "Sort time by closest time first"
-  [x y]
-  (cond
-    (t/equal? x y) 0
-    (t/after? x y) -1
-    :default 1))
-
-(defn sort-conversations
-  "Sorts conversations by the newest conversation first"
-  [convs]
-  (into (sorted-map-by (fn [x y]
-                         (time-comparator (get-in convs [x :last-update])
-                                          (get-in convs [y :last-update])))) convs))
-
-(defn normalize-data
-  [msg]
-  (update msg :date (fn [date]
-                      (when date
-                        (fmt/parse (:date-hour-minute-second-ms fmt/formatters) date)))))
+  (:require [postal-messenger.client.util.http :as http]))
 
 (defn send-event!
-  [body]
-  (http/post! "api/message" body))
+  ([socket_id type] (send-event! socket_id type nil))
+  ([socket_id type data]
+   (http/post! "/api/message" {:dest      :phone
+                               :type      type
+                               :socket_id socket_id
+                               :data      data})))
 
 (defn send-message!
   [socket_id idx conv msg]
-  (send-event! {:dest      :phone
-                :type      :send-message
-                :socket_id socket_id
-                :data      {:type       :sent
-                            :idx        idx
-                            :recipients (:recipients conv)
-                            :text       msg}}))
+  (send-event! socket_id :send-message {:type       :sent
+                                        :idx        idx
+                                        :recipients (:recipients conv)
+                                        :text       msg}))
 
 (defn get-contacts!
   [socket_id]
-  (send-event! {:dest      :phone
-                :type      :get-contacts
-                :socket_id socket_id}))
+  (send-event! socket_id :get-contacts))
+
+(defn get-conversations!
+  [socket_id]
+  (send-event! socket_id :get-conversations))
